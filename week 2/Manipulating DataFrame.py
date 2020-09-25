901ca400b99fa5f92f6fb36757b25ed7fc9b632d
Efim Zabarsky <zzfima@gmail.com>
2020-09-25 11:08:39 +0300

    rfct

--- week 2/Manipulating DataFrame.py
+++ week 2/Manipulating DataFrame.py
@@ -1,3 +1,13 @@
 import pandas as pd
 
-df = pd.read_csv('us_presidents.csv')
+
+def split_name(row):
+    row['First'] = row['president'].split(' ')[0]
+    row['Last'] = row['president'].split(' ')[-1]
+    return row
+
+
+df = pd.read_csv('us_presidents.csv', index_col=0)
+print(df.head())
+df = df.apply(split_name, axis='columns')
+print(df.head())
